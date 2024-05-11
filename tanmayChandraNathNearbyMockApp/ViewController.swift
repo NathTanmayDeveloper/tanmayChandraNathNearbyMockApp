08c0c8b6e309c420e8c194b9c57f86393fe447c9
Tanmay Chandra Nath <nathtanmaydeveloper@gmail.com>
2024-05-11 13:26:08 +0530

    Initial Commit

---
+++ tanmayChandraNathNearbyMockApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tanmayChandraNathNearbyMockApp
+//
+//  Created by Tanmay Chandra Nath on 11/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tanmayChandraNathNearbyMockAppUITests/tanmayChandraNathNearbyMockAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  tanmayChandraNathNearbyMockAppUITestsLaunchTests.swift
+//  tanmayChandraNathNearbyMockAppUITests
+//
+//  Created by Tanmay Chandra Nath on 11/05/24.
+//
+
+import XCTest
+
+final class tanmayChandraNathNearbyMockAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
